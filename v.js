aecdf88511ca05c526f5a47f3c98dfbac47f5474
ASHWINMKR <152799473+ASHWINMKR@users.noreply.github.com>
2024-08-21 14:25:08 +0530

    Add files via upload

---
+++ v.js
@@ -0,0 +1,8 @@
+function googleTranslateElementInit(){
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    console.log('google_translate_element');
+}
+if(userinput == "ashwin"){
+    alert(userinput)
+}
+
